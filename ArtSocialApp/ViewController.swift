9d0ebf9bb0ac30bd0d1b7c44d2836362d27d7973
navid-sheik <navidsheikh2020@gmail.com>
2021-09-17 15:08:17 +0100

    Initial Commit

---
+++ ArtSocialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtSocialApp
+//
+//  Created by Navid Sheikh on 17/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
